e2f46b53507f5fb65534c942171bc7b620356f0c
PerseP <persep@gmail.com>
2014-05-16 16:19:26 +0200

    Refactored count_words using inject

--- hw1/lib/fun_with_strings.rb
+++ hw1/lib/fun_with_strings.rb
@@ -4,14 +4,15 @@ module FunWithStrings
     s == s.reverse
   end
 
-  def count_words
+def count_words
     s = self.gsub(/[^\w\s]/, '')  # anything that is NOT a word character OR a space character.
     words = s.downcase.split
-    words_freq = Hash.new(0)
-    words.each { |word|
-      words_freq[word] += 1
-    }
-    words_freq
+    # The inject method takes one parameter (the object to build up, in this case a hash), 
+    # and a block specifying the action to take on each item. The block takes two parameters: 
+    # the object being built up (the hash), and one of the items from the array.
+    # The next time it calls the block, inject uses the value it got from the block the 
+    # last time it called the block, in this case the hash
+    words.inject(Hash.new(0)) { |hash,word| hash[word] += 1; hash }
   end
 
   def anagram_groups
