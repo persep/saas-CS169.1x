a83b55c4c54f415cedc497664b159fd830b59778
PerseP <persep@gmail.com>
2014-05-21 12:32:28 +0200

    Complete method for attr_accessor_with_history.rb

--- hw1/lib/attr_accessor_with_history.rb
+++ hw1/lib/attr_accessor_with_history.rb
@@ -1,10 +1,48 @@
 class Class
   def attr_accessor_with_history(attr_name)
     attr_name = attr_name.to_s # make sure it's a string
+
+    #getter
+    # def name
+	#	  @name
+	# end
+	#setter
+	# def name=(value)
+    #   @name = value
+	# end 
+
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
+
+    @previous = nil # first value of history
+
     class_eval %Q{
-      # YOUR CODE HERE
+    	# http://paul-wong-jr.blogspot.com.es/2012/04/ruby-metaprogramming-with-classeval.html
+    	
+    	def #{attr_name}=attr_name
+  
+    		# An attribute's initial value is always nil by default.
+    		# The very first time the attribute receives a value, 
+    		# its history array will have to be initialized.
+    		# Instance variables are referred with @ within getters and setters
+
+    		@#{attr_name+"_history"} = [] unless @#{attr_name+"_history"}
+    		@#{attr_name+"_history"} << @previous
+    		
+    		@#{attr_name} = attr_name # setter update
+    		
+    		@previous = attr_name # update with new value
+    	end
     }
   end
 end
+
+class Foo 
+  attr_accessor_with_history :bar
+end
+
+f = Foo.new        
+f.bar = 3          # => 3
+f.bar = :wowzo     # => :wowzo
+f.bar = 'boo!'     # => 'boo!'
+print f.bar_history.inspect      # => [nil, 3, :wowzo]
